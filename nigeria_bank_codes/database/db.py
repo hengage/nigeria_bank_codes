1ca1773a6f1341a49a73fefeefe8bda35b549c48
Josephchinedu <josephchinedu34@gmail.com>
2021-11-15 15:31:48 -0800

    addind banks

--- nigeria_bank_codes/database/db.py
+++ nigeria_bank_codes/database/db.py
@@ -205,7 +205,277 @@ def bankdb():
             "name": "One Finance",
             "bank_short_name": "",
             "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100027",
+            "cbn_code": "",
+            "name": "Mimoney Intellifin Solutions",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100028",
+            "cbn_code": "",
+            "name": "Ag Mortgage Bank",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100029",
+            "cbn_code": "",
+            "name": "Innovectives Kesh",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100031",
+            "cbn_code": "",
+            "name": "FCMB Easy Account",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100032",
+            "cbn_code": "",
+            "name": "Contec Global Infotech",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100033",
+            "cbn_code": "",
+            "name": "Palmpay",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "100052",
+            "cbn_code": "",
+            "name": "Access Yello and Beta",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110001",
+            "cbn_code": "",
+            "name": "Payattitude Online",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+        {
+            "bank_code": "110002",
+            "cbn_code": "",
+            "name": "Flutterwave Tech Solutions",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110003",
+            "cbn_code": "",
+            "name": "Interswitch",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110004",
+            "cbn_code": "",
+            "name": "First Apple",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110005",
+            "cbn_code": "",
+            "name": "3Line",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110006",
+            "cbn_code": "",
+            "name": "Paystack Payments",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "110007",
+            "cbn_code": "",
+            "name": "TeamApt",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "400001",
+            "cbn_code": "501",
+            "name": "FSDH Merchant Bank",
+            "bank_short_name": "fsdh",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090270",
+            "cbn_code": "",
+            "name": "Ab MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "070010",
+            "cbn_code": "",
+            "name": "Abbey Mortgage Bank",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090260",
+            "cbn_code": "",
+            "name": "Above Only MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090197",
+            "cbn_code": "",
+            "name": "Abu MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "000014",
+            "cbn_code": "044",
+            "name": "Access Bank Nigeria",
+            "bank_short_name": "access",
+            "disabled_for_vnuban": False
+        },
+
+        {
+            "bank_code": "090134",
+            "cbn_code": "",
+            "name": "Accion MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090160",
+            "cbn_code": "",
+            "name": "Addosser MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090268",
+            "cbn_code": "",
+            "name": "Adeyemi College Staff MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+        {
+            "bank_code": "090292",
+            "cbn_code": "",
+            "name": "Afekhafe MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090133",
+            "cbn_code": "",
+            "name": "Al-Barkah MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090277",
+            "cbn_code": "",
+            "name": "Al-Hayat MFB",
+            "bank_short_name": "",
+            "disa,bled_for_vnuban": None
+        },
+        {
+            "bank_code": "090259",
+            "cbn_code": "",
+            "name": "Alekun MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+        {
+            "bank_code": "090297",
+            "cbn_code": "",
+            "name": "Alert MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090131",
+            "cbn_code": "",
+            "name": "Allworkers MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090169",
+            "cbn_code": "",
+            "name": "Alpha Kapital MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090180",
+            "cbn_code": "",
+            "name": "Amju Unique MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090116",
+            "cbn_code": "",
+            "name": "AMML MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090143",
+            "cbn_code": "",
+            "name": "Apeks MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
+        },
+
+        {
+            "bank_code": "090282",
+            "cbn_code": "",
+            "name": "Arise MFB",
+            "bank_short_name": "",
+            "disabled_for_vnuban": None
         }
+
+
 
         
 
